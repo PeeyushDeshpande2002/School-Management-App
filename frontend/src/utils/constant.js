aa0a7276fd7fc1ab6630c1d820f478df09d781f6
PeeyushDeshpande2002 <deshpp2002@gmail.com>
2024-12-20 15:29:09 +0530

    .

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,8 +1,10 @@
+const API = 'https://school-management-app-bv25.onrender.com/api'
+
 export const USER_API_ENDPOINT = `${API}/user`
 export const CLASS_API_ENDPOINT = `${API}/class`
 export const ADMIN_API_ENDPOINT = `${API}/admin`
 export const STUDENT_API_ENDPOINT = `${API}/student`
 export const TEACHER_API_ENDPOINT = `${API}/teacher`
 
-const API = 'https://school-management-app-bv25.onrender.com/api'
+
 //http://localhost:8000/api
